85c890e90cbc2a8694d83bb2265cd925dac2fac1
Eric Dacal <ericdacal@gmail.com>
2017-04-09 01:11:32 +0200

    Eric's Bloom filter created and inclusion of pdfs

---
+++ Bloom_Eric.cc
@@ -0,0 +1,28 @@
+
+// Guarda el fichero con algun otro nombre para no generar conflictos con el del resto (como Bloom_Eric.cc)
+// Esto, pero, implicará hacer cambios en el Makefile!
+
+#include <iostream>
+#include <string>
+
+int hash(std::string key, int m) {
+    int out = 0;
+    for(int i = 0; i < key.length();++i) {
+        if(key[i] == 'a' and key[i] == 'z') out += int(key[i] - 'a');
+        else if(key[i] == 'A' and key[i] == 'Z') out += int(key[i] - 'z');
+        else out += key[i];
+    }
+    return (out % m);
+}
+
+
+int main () {
+	//Code
+  std::string key;
+  int m;
+  std::cout << "Introduce la clave a insertar en el conjunt: " << std::endl;
+  std::cin >> key;
+  std::cout << "Introduce el tamaño del filtro de Bloom: " << std::endl;
+  std::cin >> m;
+  std::cout << hash(key, m) << std::endl; 
+}
